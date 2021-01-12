33b7c3e89363e4d979a27db97d311077272bdb0e
unknown <samarth.vrl@gmail.com>
2021-01-12 15:21:28 +0530

    Initial code for leetcode program

--- Programs/Algorithms/examples.js
+++ Programs/Algorithms/examples.js
@@ -1,92 +1,35 @@
 /**
- * @param {number[]} nums
- * @param {number} k
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
  * @return {number}
  */
-let findKthLargest = function(nums, k) {
+let findTilt = function(root) {
+    
+    if(root === null)
+        return;
 
-    let result = 0;
-    let heap = new MaxHeap();
-    nums.forEach(item => {
-      heap.insert(item);
-    });
+    let diff = Math.abs(this.findTilt(root.left) - this.findTilt(root.right));
+    root.data = diff;
+};
 
-    while(k) {
-      result = heap.remove();
-      k--;
-    }
-    return result;
-}
-
-class MaxHeap {
-
-    constructor(heap = []) {
-        this.heap = heap;
+let totaltilt = 0;
+let valueSum = function(node) {
+    
+    if(node === null) {
+        return 0;
     }
 
-    insert = (value) => {
-        this.heap.push(value);
-        let index = this.heap.length - 1;
-        let parent = this.parent(index);
-
-        while(index > 0 && this.heap[index] > this.heap[parent]) {
-            [this.heap[index], this.heap[parent]] = [this.heap[parent], this.heap[index]];
-            index = parent;
-            parent = this.parent(index);
-        }
-    }
-
-    remove = () => {
-        let smallest = this.getMin();
-
-        if(this.heap.length > 1) {
-
-            this.heap[0] = this.heap[this.heap.length - 1];
-            this.heap.splice(this.heap.length - 1);
-
-            let index = 0;
-            let left = this.leftChild(index);
-            let right = this.rightChild(index);
-
-            while(this.heap[left] !== undefined && this.heap[right] !== undefined && this.heap[left] >= this.heap[index] || this.heap[right] >= this.heap[index]) {
-                
-                if(this.heap[left] > this.heap[right]) {
-                    [this.heap[left], this.heap[index]] = [this.heap[index], this.heap[left]];
-                    index = left;
-                } else {
-                    [this.heap[right], this.heap[index]] = [this.heap[index], this.heap[right]];
-                    index = right;
-                }
-
-                left = this.leftChild(index);
-                right = this.rightChild(index);
-            }
-
-            if(this.heap[right] === undefined && this.heap[left] > this.heap[index]) {
-                [this.heap[left], this.heap[index]] = [this.heap[index], this.heap[left]];
-            }
-
-        } else if(this.heap.length === 1) {
-            this.heap.splice(0,1);
-        } else {
-            return null;
-        }
-        return smallest;
-    }
-
-    parent = (index) => {
-        return Math.floor((index - 1) / 2);
-    }
-
-    leftChild = (index) => {
-        return 2 * index + 1;
-    }
-
-    rightChild = (index) => {
-        return 2 * index + 2;
-    }
-
-    getMin = () => {
-        return this.heap[0];
-    }
+    let leftsum = valueSum(node.left);
+    let rightsum = valueSum(node.right);
+    let tilt = Math.abs(leftsum - rightsum);
+    totaltilt += tilt;
+    
+    return node.data;
 }

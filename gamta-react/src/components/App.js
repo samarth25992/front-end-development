fcbdbbc1c613a12ba988422facc304bb03702b66
unknown <samarth.vrl@gmail.com>
2021-01-26 17:17:11 +0530

    First react project

---
+++ gamta-react/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoItem from './TodoItem';
+
+export default function App() {
+  return (
+    <div>
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </div>
+  )
+}
---
+++ gamta-react/src/components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodoItem = () => {
+    return (
+        <div>      
+            <input type="checkbox" />
+            <p>This is the first item</p>
+        </div>
+    )
+}
+
+export default TodoItem
